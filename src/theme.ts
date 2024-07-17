7831e19ee65d2ce9936a04e5c153236fc3d6055e
Joao Francisco <joaofrancisco21@yahoo.com.br>
2024-07-17 17:33:28 -0300

    First commit

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export const colors = {
+  brand: {
+    primary: '#EF5241',
+    secondaryLight: '#544F4D',
+    secondaryDark: '#EDF2F7',
+  }
+}
+
+export const theme = extendTheme({colors})
